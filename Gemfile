16465800ff452193506a41bd2ed407680738de82
Maciej Kurek <maciejk77@gmail.com>
2015-04-24 10:42:34 +0100

    Stage 0 completed

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'https://rubygems.org'
+
+group :test do
+  gem 'rspec'
+end
+
+gem 'data_mapper'
+gem 'dm-postgres-adapter'
---
+++ lib/link.rb
@@ -0,0 +1,8 @@
+# Link class
+class Link
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :title, String
+  property :url, String
+end
